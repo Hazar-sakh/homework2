a739037f8f67227f2df942bea5c86b4719782641
Nikita <niknakels@gmail.com>
2024-06-09 00:24:50 +1100

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,6 @@
+completed_homeworks = 12
+spent_hours = 1.5
+course_name = 'Python'
+time_for_1_tusk = spent_hours/completed_homeworks
+print('Курс: ' + course_name + ', ' + 'всего задач:' + str(completed_homeworks) + ', затрачено часов: ' +
+      str(spent_hours) + ', среднее время выполнения ' + str(time_for_1_tusk) + ' часа')
