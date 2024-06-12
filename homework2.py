4a089d70f1af5f59b284f56a3ed0819b8a004424
Nikita <niknakels@gmail.com>
2024-06-12 14:36:16 +1100

    homework2.py

--- homework2.py
+++ homework2.py
@@ -1,6 +1,6 @@
-completed_homeworks = 12
-spent_hours = 1.5
-course_name = 'Python'
-time_for_1_tusk = spent_hours/completed_homeworks
+completed_homeworks = 12 # значение переменной 1
+spent_hours = 1.5 # значениепеременной 2
+course_name = 'Python' # значение переменной 3
+time_for_1_tusk = spent_hours/completed_homeworks # расчитываем среднее арифметическое
 print('Курс: ' + course_name + ', ' + 'всего задач:' + str(completed_homeworks) + ', затрачено часов: ' +
-      str(spent_hours) + ', среднее время выполнения ' + str(time_for_1_tusk) + ' часа')
+      str(spent_hours) + ', среднее время выполнения ' + str(time_for_1_tusk) + ' часа') # выводим значения с переводом типа данных int (целое число) в тип данных str (строка)
